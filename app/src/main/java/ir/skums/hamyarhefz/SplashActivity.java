78a1f4e92d10a8f7eee9637e6eb0b09faba0195a
Hatef Karimi <karimi.hatef@gmail.com>
2022-01-11 06:04:23 +0330

    process 95% debug v3

--- app/src/main/java/ir/skums/hamyarhefz/SplashActivity.java
+++ app/src/main/java/ir/skums/hamyarhefz/SplashActivity.java
@@ -1,29 +1,17 @@
 package ir.skums.hamyarhefz;
 
-import android.annotation.SuppressLint;
-
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.WindowInsets;
 
-import ir.skums.hamyarhefz.databinding.ActivityFullscreenBinding;
+public class SplashActivity extends AppCompatActivity {
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
-public class FullscreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fullscreen);
+        setContentView(R.layout.activity_splash);
+
         Thread th = new Thread() {
             @Override
             public void run() {
