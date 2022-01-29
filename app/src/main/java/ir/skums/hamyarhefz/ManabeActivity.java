11d3a43791061e2f15cb1cd4ca1d84738b60550e
Hatef Karimi <karimi.hatef@gmail.com>
2022-01-29 13:10:09 +0330

    process 99.5% debug v7

--- app/src/main/java/ir/skums/hamyarhefz/ManabeActivity.java
+++ app/src/main/java/ir/skums/hamyarhefz/ManabeActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class ManabeActivity extends AppCompatActivity {
-    TextView txtMatn,txtTitle;
+    TextView txtMatn,txtTitle,txtmtn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +15,12 @@ public class ManabeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_manabe);
         txtMatn=findViewById(R.id.textManabeQ);
         txtTitle=findViewById(R.id.txtManabeTitle);
+        txtmtn2=findViewById(R.id.txt_parhizgar);
 
 
         Typeface type=Typeface.createFromAsset(getAssets(), "fonts/b_nazanin.TTF");
         txtMatn.setTypeface(type);
         txtTitle.setTypeface(type);
+        txtmtn2.setTypeface(type);
     }
 }
